71fcd74eade52f55294b7064693362e5a86c3807
Maksim <mmonkin322@mail.ru>
2019-11-11 20:05:55 +0600

    lab-4

---
+++ src/SimpleInterihance.java
@@ -0,0 +1,22 @@
+class SimpleInterihance {
+    public static void main (String args[]) {
+        A superOb = new A();
+        B subOb = new B();
+        superOb.i = 10;
+        superOb.j = 20;
+        System.out.println("Содержимое объекта superOb: ");
+        superOb.showij();
+        System.out.println();
+
+        subOb.i = 7;
+        subOb.j = 8;
+        subOb.k = 9;
+        System.out.println("Содержимое объекта suOb: ");
+        subOb.showij();
+        subOb.showk();
+
+        System.out.println();
+        System.out.println("Сумма i, j и k в объекта subOb");
+        subOb.sum();
+    }
+}
